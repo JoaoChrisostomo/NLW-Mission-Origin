206169ac2e971d1da35c4b23d357dcf12c1777b5
JoaoChrisostomo <maturandomassas@gmail.com>
2022-05-28 03:48:24 -0300

    subindo refatorações no html e no css mudando cores de todo html através de uma variável, utilizando o plugin scrollReveal no qual os elementos vão aparecendo conforme vou scrollando a página

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
+window.addEventListener('scroll', onScroll)
+
 function onScroll() {
-    if (scrollY > 100) {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+    if (scrollY > 0) {
         navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
     }
 }
-onScroll();
+
+function showBackToTopButtonOnScroll() {
+    console.log(scrollY)
+    if (scrollY > 500) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
 
 function openMenu() {
     document.body.classList.add('menu-expanded')
