328fec64a234e13e29325415502275ff7bbc4d87
JoaoChrisostomo <maturandomassas@gmail.com>
2022-05-28 00:56:54 -0300

    Subindo o primeiro commit do projeto nesta etapa foi feito refatoração do html por sections, foi feito a parte de cards e about

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function onScroll() {
+    if (scrollY > 100) {
+        navigation.classList.add('scroll')
+    } else {
+        navigation.classList.remove('scroll')
+    }
+}
+onScroll();
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    #home,
+    #home .logo-menu,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about .content`);
